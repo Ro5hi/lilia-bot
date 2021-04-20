211fa2093e07eaddacffd3c6253e6e2edbc564c8
Ro5hi <slen.detablan@gmail.com>
2021-04-20 17:34:24 -0600

    added words array for new command

--- test-bot.py
+++ test-bot.py
@@ -18,14 +18,22 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    msg = message.content
 
-    if message.content.startswith('/boba'):
+    if msg.startswith('/boba'):
         await message.channel.send('Finish your boba!')
         
-    if message.content.startswith('baaa'):
+    if msg.startswith('baaa'):
       await message.channel.send('Baaaa~')
       
-    if message.content.startswith('/nya'):
+    if msg.startswith('/nya'):
         await.message.channel.send('No free nyas, sorry~')
+        
+    if msg.startswith('ara ara'):
+        await.message.channel.send(Nope)
+        
+    if any(word in msg for word in Nope):
+        await.message.channel.send(random.choice(Bad))
 
 client.run(os.getenv('TOKEN'))
