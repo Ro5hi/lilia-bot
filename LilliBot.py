a774276a0b4a84aee0532e5094050e20a5c10eaf
Ro5hi <slen.detablan@gmail.com>
2021-04-20 18:21:22 -0600

    testing uptimerobot with lilibot on discord

--- LilliBot.py
+++ LilliBot.py
@@ -1,7 +1,21 @@
 import discord
 import os
+import requests
+import json
+import random
+from keep_alive import keep_alive
+
 
 client = discord.Client()
+
+Nope = ["/ara", "ara ara"]
+
+Bad = ["pain", "NO", "None for you", "Never!"]
+
+# def get_twitch():
+#   response = requests.get("https://twitch.tv/deepsealily")
+#   json_data = json.loads(response.text)
+#   notif = json_data[0][t] 
 
 @client.event
 async def on_ready():
@@ -12,7 +26,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('/boba'):
-        await message.channel.send('Finish your boba!')
+    msg = message.content
 
+    if msg.startswith('/boba'):
+        await message.channel.send('Finish your boba!')
+    
+    if msg.startswith('baaa'):
+      await message.channel.send('Baaaa~')
+
+    if msg.startswith('/nya'):
+      await message.channel.send('No free nyas, sorry~')
+
+    if any(word in msg for word in Nope):
+      await message.channel.send(random.choice(Bad))
+  
+keep_alive()
 client.run(os.getenv('TOKEN'))
